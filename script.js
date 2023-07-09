a827f26581cfef6a7cee82e9b52ab590247e042f
sunnidaze7 <pnykii98@gmail.com>
2023-07-09 12:25:22 -0500

    comppleted project

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Write your code here
+//selectors
+let lightbulb1 = document.querySelector('#lightbulb1')
+let lightbulb2 = document.querySelector('#lightbulb2')
+let lightbulb3 = document.querySelector('#lightbulb3')
+let subtitle = document.querySelector('.subtitle')
+
+let count = 0
+
+lightbulb1.addEventListener('click', function(){
+    count++
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+    this.classList.toggle('active')
+})
+
+lightbulb2.addEventListener('click', function(){
+    count++
+    subtitle.innerHTML =`You've clicked the lights ${count} times`
+    this.classList.toggle('active')
+})
+
+lightbulb3.addEventListener('click', function(){
+    count++
+    subtitle.innerHTML = `You've clicked the lights ${count} times`
+    this.classList.toggle('active')
+})
